8e5b182df112383c96e455900b18c10db8bb74d6
rgt <stihinaleksander@yandex.ru>
2021-10-24 10:43:51 +0500

    initial

---
+++ app/src/main/java/com/example/sirius_part1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sirius_part1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
+import kotlin.time.Duration
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
